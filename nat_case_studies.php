c168138c852b1eb6d31d10ec40574b7d71cfd1ca
Captain n0z3ro <admin@n0z3ro.com>
2018-05-01 17:18:31 -0400

    adding support for new meta fields

--- nat_case_studies.php
+++ nat_case_studies.php
@@ -1,13 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
  * @link              http://n0z3ro.com/
  * @since             1.0.0
  * @package           Nat_case_studies
@@ -15,7 +8,7 @@
  * @wordpress-plugin
  * Plugin Name:       Nature Case Studies
  * Plugin URI:        http://naturestudio.us/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Creates the Case Studies post type
  * Version:           1.0.0
  * Author:            Clint Ford
  * Author URI:        http://n0z3ro.com/
@@ -26,20 +19,14 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'NATURE_CASE_STUDIES_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-nat_case_studies-activator.php
  */
 function activate_nat_case_studies() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-nat_case_studies-activator.php';
@@ -48,7 +35,6 @@ function activate_nat_case_studies() {
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-nat_case_studies-deactivator.php
  */
 function deactivate_nat_case_studies() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-nat_case_studies-deactivator.php';
@@ -66,17 +52,186 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-nat_case_studies.php';
 
 /**
  * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
  * @since    1.0.0
  */
 function run_nat_case_studies() {
-
 	$plugin = new Nat_case_studies();
 	$plugin->run();
 
+	//hooks
+	add_action('init', 'nat_case_studies_custom_post_type');
+	add_action( 'add_meta_boxes', 'nat_custom_meta_boxes' );
+	add_action( 'save_post', 'nat_save_meta_box' );
 }
+
+function nat_case_studies_custom_post_type() {
+	register_post_type('nat_case_studies',
+		array(
+				'labels' => array(
+				'name' => __('Case Studies'),
+				'singular_name' => __('Case Study'),
+			),
+			'supports' => array('title', 'editor', 'thumbnail'),
+			'public' => true,
+			'has_archive' => true,
+		)
+	);
+
+	//Custom Taxonomies
+	$prj_type_labels = [
+		'name' => _x('Project Type', 'taxonomy general name'),
+		'singular_name' => _x('Project Type', 'taxonomy singular name'),
+		'search_items' => __('Search Project Types'),
+		'all_items' => __('All Project Types'),
+		'parent_item' => __('Parent Project Type'),
+		'parent_item_colon' => __('Parent Project Type:'),
+		'edit_item' => __('Edit Project Type'),
+		'update_item' => __('Update Project Type'),
+		'add_new_item' => __('Add New Project Type'),
+		'new_item_name' => __('New Project Type'),
+		'menu_name' => __('Project Type'),
+	];
+	$prj_type_args = [
+		'hierarchical' => true,
+		'labels' => $prj_type_labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => ['slug' => 'project-type'],
+	];
+	register_taxonomy('project-type', ['nat_case_studies'], $prj_type_args);
+
+	$ser_labels = [
+		'name' => _x('Services', 'taxonomy general name'),
+		'singular_name' => _x('Service', 'taxonomy singular name'),
+		'search_items' => __('Search Services'),
+		'all_items' => __('All Services'),
+		'parent_item' => __('Parent Service'),
+		'parent_item_colon' => __('Parent Service:'),
+		'edit_item' => __('Edit Service'),
+		'update_item' => __('Update Service'),
+		'add_new_item' => __('Add New Service'),
+		'new_item_name' => __('New Service Name'),
+		'menu_name' => __('Services'),
+	];
+	$ser_args = [
+		'hierarchical' => true,
+		'labels' => $ser_labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => ['slug' => 'service'],
+	];
+	register_taxonomy('service', ['nat_case_studies'], $ser_args);
+
+	$del_labels = [
+		'name' => _x('Deliverables', 'taxonomy general name'),
+		'singular_name' => _x('Deliverable', 'taxonomy singular name'),
+		'search_items' => __('Search Deliverables'),
+		'all_items' => __('All Deliverables'),
+		'parent_item' => __('Parent Deliverable'),
+		'parent_item_colon' => __('Parent Deliverable:'),
+		'edit_item' => __('Edit Deliverable'),
+		'update_item' => __('Update Deliverable'),
+		'add_new_item' => __('Add New Deliverable'),
+		'new_item_name' => __('New Deliverable Name'),
+		'menu_name' => __('Deliverables'),
+	];
+	$del_args = [
+		'hierarchical' => true,
+		'labels' => $del_labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => ['slug' => 'deliverable'],
+	];
+	register_taxonomy('deliverable', ['nat_case_studies'], $del_args);
+
+	//fixes permalink 404 errors - DISABLE IN FINAL RELEASE
+	flush_rewrite_rules( false );
+}
+
+function nat_custom_meta_boxes() {
+	//Add Meta Boxes
+	add_meta_box( 'project-id-meta-box', __( 'Project Name', 'textdomain' ), 'nat_project_meta', 'nat_case_studies', 'side', 'high' );
+
+	function nat_project_meta( $post ) {
+		// Display code/markup goes here. Don't forget to include nonces!
+		wp_nonce_field( basename( __FILE__ ), 'nat_nonce' );
+		$nat_stored_meta = get_post_meta( $post->ID );
+		?>
+
+		<p>
+			<input type="text" name="nat-project" id="nat-project" value="<?php if(isset($nat_stored_meta['nat-project'])) echo $nat_stored_meta['nat-project'][0]; ?>" />
+		</p>
+
+		<?php
+	}
+
+}
+
+function nat_save_meta_box( $post_id ) {
+
+	// Checks save status
+	$is_autosave = wp_is_post_autosave( $post_id );
+	$is_revision = wp_is_post_revision( $post_id );
+	$is_valid_nonce = ( isset( $_POST[ 'nat_nonce' ]) && wp_verify_nonce( $_POST[ 'nat_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+	// Exits script depending on save status
+	if( $is_autosave || $is_revision || !$is_valid_nonce ) {
+		return;
+	}
+
+	// Checks for input and sanitizes/saves if needed
+	if( isset( $_POST[ 'nat-project' ])) {
+		update_post_meta( $post_id, 'nat-project', sanitize_text_field( $_POST[ 'nat-project' ]));
+	}
+
+}
+
 run_nat_case_studies();
+
+/* MEDIA UPLOAD TEST TEST TEST */
+//Assign a name to your tab
+function upload_test_media_menu($tabs) {
+	$tabs['test_upload']='Test Upload';
+	return $tabs;
+}
+//Adds your scripts to your plugin
+function upload_test_scripts() {	
+	//Adds css
+	$myStyleUrl = plugins_url('upload-media.css', __FILE__);
+	wp_register_style('myStyleSheets', $myStyleUrl);
+	wp_enqueue_style( 'myStyleSheets');
+	//Adds JQuery 
+	wp_deregister_script('jquery');
+  	wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+  	wp_enqueue_script( 'jquery' );
+	//Adds your custom script
+	wp_deregister_script( 'upload-media-script' );
+	wp_register_script( 'upload-media-script', '/wp-content/plugins/upload-media/upload-media.js');
+	wp_enqueue_script( 'upload-media-script' );
+}
+//This is our form for the plugin
+function upload_test_upload_form () {
+	//echos the tabs at the top of the media window
+	media_upload_header();
+	//Adds your javascript
+	upload_test_scripts();
+	?>
+	<div class="test-form">
+		<input type='text' id='name' />
+		<input id='insert_shortcode' type='button' class='button' value='Insert Shortcode'>
+	</div> 	
+
+<?php }
+//Returns the iframe that your plugin will be returned in
+function upload_test_menu_handle() {
+	return wp_iframe('upload_test_upload_form');
+}
+//Needed script to make sure wordpresses media upload scripts are inplace
+wp_enqueue_script('media-upload');
+//Adds your tab to the media upload button
+add_filter('media_upload_tabs', 'upload_test_media_menu');
+//Adds your menu handle to when the media upload action occurs
+add_action('media_upload_test_upload', 'upload_test_menu_handle');
